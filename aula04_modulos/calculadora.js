bd9007d7ddd619993cbecd57fb833da64751b8ff
andrefmuller <andrefredericomuller@gmail.com>
2021-11-17 17:30:25 -0300

    Initial commit

---
+++ aula04_modulos/calculadora.js
@@ -0,0 +1,13 @@
+var FuncSoma = require("./somar")
+var FuncSubtrai = require("./subtrair")
+var FuncMultiplica = require("./multiplicar")
+var FuncDivide = require("./dividir")
+
+
+console.log(FuncSoma(10,4))
+
+console.log(FuncSubtrai(17,12))
+
+console.log(FuncMultiplica(6,6))
+
+console.log(FuncDivide(10,4))
---
+++ olamundo/nome.js
@@ -0,0 +1,4 @@
+var nome = `André Frederico`
+var sobrenome = `Müller`
+
+console.log(nome + " " + sobrenome)
