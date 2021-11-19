0ab96071140f50c3739a712bc049e03e05f84887
andrefmuller <andrefredericomuller@gmail.com>
2021-11-19 18:31:37 -0300

    Aulas 19/11/2021

--- aula19_handlebars/index.js
+++ aula19_handlebars/index.js
@@ -1,29 +1,24 @@
-const express = require("express")
+const express = require(`express`)
 const app = express()
-const handlebars = require("express-handlebars")
-const Sequelize = require("sequelize")
+const hbr = require(`handlebars`)
+const hbs = handlebars.create({defaultLayout:`main`}) 
+const Sequelize = require(`sequelize`)
 
 //Config
     //Template Engine
-    app.engine("handlebars", handlebars({defaultLayout: "main"}))
-    app.set("view engine","handlebars")
-//
+    app.engine(`handlebars`, () => hbs)
+    app.set(`view engine`,`handlebars`)
 //Conexão com o banco de dados MariaDB
-const sequelize = new Sequelize("teste","admin","admin", {
-    host: "localhost",
-    dialect: "mariadb"
-})
+    sequelize = new Sequelize("teste","admin","admin", {
+        host: "localhost",
+        dialect: "mariadb"
+    })
 
-
-
-
-sequelize.authenticate().then(function(){
-    console.log("Conectado com sucesso!")
-}).catch(function(erro){
-    console.log("Falha ao se conectar: "+erro)
-})
+//Rotas
+    app.get('/cad', function(req,res){
+        res.send(`ROTA DE CADASTRO DE POSTS`)
+    })
 
 app.listen(8090, function(){
     console.log("Servidor executando na porta 8090.")
-    console.log("http://localhost:8090")
 })
